22df310771e5300421289754ce0eccc353254ccd
VictorBrito13 <javibrito10@gmail.com>
2025-01-12 15:47:07 -0600

    fix: Fix screen overflow problem

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
   }, [])
 
   return (
-    <div className='container justify-content-center d-grid d-md-flex'>
+    <div className='container p-0 justify-content-center d-grid d-md-flex'>
       {/* Desserts Container */}
       <div className='flex-grow-1'>
         <h1 className='f-red-hat-bold'>Desserts</h1>
@@ -32,6 +32,7 @@ function App() {
         {/* List of desserts */}
       </div>
       {/* Cart */}
+      <Cart/>
     </div>
   )
 }
